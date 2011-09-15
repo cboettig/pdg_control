ac56c6acab0f5a7e6ac9d002d19994dff9bc8751
Carl Boettiger <cboettig@gmail.com>
2011-09-15 01:02:32 -0400

    helps to add fishing and cost of information to get nontrivial solutions

--- demo/training_prob2.R
+++ demo/training_prob2.R
@@ -2,19 +2,23 @@
 
 # population growth function: Discrete time Ricker with Allee effect
 # Returns the function f as a closure
-set_f <- function(gamma, C, K){ 
-  function(x) x * exp( gamma*(1-x/K)*((x-C)/K))
+set_f <- function(gamma, C, K, beta){ 
+  function(x, E_h) x * exp( gamma*(1-x/K)*((x-C)/K)) - x*E_h*beta
 }
-# Initialize 
-f <- set_f(1, 20, 100)
+
+# Initialize f.  Allee threshold 20, carrying capacity 100, .1 efficacy 
+# (e.g. effort = 1/beta means catch all the stock)
+f <- set_f(1, 20, 100, .1)
+
+
 
 # Recursively apply f for t years
-F <- function(N,t){ 
+F <- function(N,t, E_h){ 
   i = 1
   n <- N
   iterate_t <- function(t){
     while(i<=t){ 
-      n <- f(n)
+      n <- f(n, E_h)
       i<- i+1
     }
     n
@@ -23,45 +27,62 @@ F <- function(N,t){
 }
 
 t <-1:20
-plot(t, F(22,t))
+plot(t, F(50,t,1))
 
 
+info_cost <- 1
 
-Pi <- function(E_t, E_s, t){
+
+Pi <- function(E_h, E_s, t){
 # Define the initial probability distribution of population density
-# SD comes at a given cost, sd = 1/E_s
-  P_0 <- function(x) dnorm(x, mean=60, sd=1/E_s) 
+# Increased sampling effort reduces the the variance 
+  P_0 <- function(x) dlnorm(x, mean=log(60), sd=1/E_s)
 
   iterate_t <- function(t){ # in case t is a vector
-    int <- function(N) E_t*N*F(N,t)*P_0(N)
-    integrate(int, 0, 200)$value
+    int <- function(N) E_h*N*F(N,t,E_h)*P_0(N)
+    sum(sapply(0:200, int))
+#    integrate(int, 0, 200)$value
   }
-  sapply(t, iterate_t)
+
+  info_cost_per_time <- function(t){
+# pay only on first time interval
+    out<- numeric(length(t))
+    out[1] <- E_s*info_cost
+    out
+  }
+
+  sapply(t, iterate_t) - info_cost_per_time(t)
 }
 
 
 r <- 1 # discounting (interest) rate
+T <- 20 # time horizon
 
 # target function which we optimize
 target <- function(pars){
-  int <- function(t) Pi(pars["E_t"], pars["E_s"], t)*exp(-r*t)
-  -integrate(int, 0, T)$value
+  int <- function(t) Pi(pars["E_h"], pars["E_s"], t)*exp(-r*t)
+  -sum(sapply(1:T, int))
+#  -integrate(int, 0, T)$value
+}
+
+E_s <- seq(1, 20,length=6)
+E_h <- seq(1, 10, length=10)
+## E_h rows, E_s cols
+gridsearch <- function(E_s, E_h){
+  out<-sapply(E_s, function(y){
+    sapply(E_h, function(x){
+      pars <- c(E_h=x,E_s=y)
+      target(pars)
+    })
+  })
+  out
 }
 
 
-E_s <- seq(.1, 2.1, by=.5)
-E_t <- seq(.1, 2.1, by=.5)
 
-out<-sapply(E_s, function(y){
-  sapply(E_t, function(x){
-    pars <- c(E_t=x,E_s=y)
-    target(pars)
-  })
-})
+pars <- c(E_h=5, E_s=5)
+o <- optim(pars, target)
 
-# Whoops, solution is to harvest at maximum effort and spend nothing on sampling
-
-#optim(pars, target)
-
-
+#optimal solution is:
+o$par
 
