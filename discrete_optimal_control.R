4985215a7cd24d9297528c04d1cc8e196c957174
Carl Boettiger <cboettig@gmail.com>
2011-10-28 17:15:19 -0700

    trouble in discreteland

--- discrete_optimal_control.R
+++ discrete_optimal_control.R
@@ -1,64 +1,108 @@
-# file: training_prob2_collocation.R
+# file: training_prob2_collocation.U
 # author: Carl Boettiger, \url{http://carlboettiger.info}
 # date: 2011-11-27
 
 
+
+# \[ k_{t+1} = k_t + f(k_t) - h_t \]
+# F(x) = f(k) + k
+#
+# Discount rate \beta < 1
+# \[ \max_{h_t} \sum_{t=0}^{\infty} \beta^t U(h_t)  \]
+# s.t. \( k_{t+1} = F(k_t) -h_t \)
+# k_0 = k0
+
+
+# First-order conditions are:
+#\[ U'(h_t) = \beta U'(h_{t+1}) F'(k_{t+1} ) \forall t \]
+
+# \[ V(k) = \max_h U(h) + \beta V(k_{t+1} ) \]
+# \[ V(k) = \max_h U(h) + \beta V( F(k) - h) \]
+
+
+
+rm(list=ls()) # start clean
+
 alpha <- 1
 K <- 1
-r <- 1
-T<- 1
+C <- 0.2
+beta <- 0.95 # discounting rate (discrete)
+gamma <- -2
+T <- 10
 
-# Discrete time state equation.  BH-style
-# \[ x_{t+1} = x_t + f(t, x_t, u_t) \] 
-f <- function(t, x, u){
-  r * x ^ alpha / (1 + x ^ alpha / K) - x * u
+# Discrete time state equation.  Ricker-style
+# \[ x_{t+1} = f(t, x_t, h_t) \] 
+f <- function(t, x, h){
+  if(is.na(h)) 
+    recover()
+#  r * x ^ alpha / (1 + x ^ alpha / K) - x * h ## B-H style
+  x * exp(alpha * (1 - x / K) * (x - C) / K ) - h * x
 }
 
-# Define the utility function R
-R <- function(t, x, u){
-  gamma <- -2
-  u ^ (1+gamma) / (1 + gamma)
+
+
+
+# Define the utility function U
+U <- function(t, x, h){
+  h ^ (1+gamma) / (1 + gamma)
 }
 
 # and the boundary condition cost \Phi
-Phi <- function(x) (x != XT)*Inf 
+phi <- function(x) if(x > 0.2) 0 else 1e6
 
-
-
-
-
-# Define the Cost function
-# \[ C(x_0, u(t)) = \Phi(x_T) + \sum_{t=0}^{T-1} R(t,x_t, u_t) \]
-C <- function(x, u){
-  Phi(x[length(x)]) + 
-  sum(sapply(1:length(t), function(i) R(t[i], x[i], u[i])))
+Phi <- function(x) {
+  sapply(x, 
+    function(X){
+      if(is.na(X))
+        out <- Inf
+      else if(X > .2) 
+        out <- 0
+      else
+        out <- Inf
+      out
+    })
 }
 
-# The discrete time grid
-#t <- seq(0, T, length.out = 10)
-
-#U <- matrix(NA, ncol=2, nrow=length(t))
-#J <- matrix(NA, ncol=length, nrow=length(t))
-
-#J[
-
-
-t = T-1.  x =
-
-
-
-for(t in (T-1):0){
-  for(x_t in x){
-
-   <- R(t, x, u[i], 
-
-  }
+J <- function(t,x){
+  if(t < T)
+    out <- U(t,x, h_star(t,x)) + beta*J(t+1, f(t,x, h_star(t,x)))
+  else 
+    out <- phi(x)
+  out
+}
+  
+h_star <- function(t,x){
+    func <- function(h) U(t, x, h) + beta*J(t+1, f(t,x,h))
+#    optimize(f=func, interval=c(0,1))[[1]]
+    h <- seq(0,1,length=10)
+    i <- which.max( sapply(h, func) )
+    if(is.na(h[i])){
+      print(paste("failed i=", i))
+      recover()
+      }
+    h[i]
 }
 
-
-# Define the optimal cost-to-go:
-#\[ J(t, x_t) = \min_{u_{t:T-1}}  C(x_t, u_{t:T-1} )  \]
-J <- function
-
-
+#
+#### Constant fishing effort check  ###
+#y <- numeric(T)
+#y[1] <- K # constraint/initial condition
+#for(t in 1:(T-1))
+# y[t+1] = f(t,y[t], .2)
+#plot(1:T, y, pch=19)
+#
+#
+### Optimal Control Solution ##
+#
+#y <- numeric(T)
+#y[1] <- K # x_0 constraint/initial condition
+#h <- numeric(T)
+#for(t in 1:(T-1)){
+#  h[t] <- h_star(t, y[t])  
+# y[t+1] = f(t,x[t], h[t])
+#}
+#plot(1:T, y, pch=19)
+#points(1:T, h, pch=19, col="red")
+#
+#
 
