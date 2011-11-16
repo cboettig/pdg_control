c61573c36ee14a8687f82984a1692d57ad535704
Carl Boettiger <cboettig@gmail.com>
2011-11-16 09:43:46 -0800

    clean up, seperated out extra plots

--- R/SDP/Reed.R
+++ R/SDP/Reed.R
@@ -11,11 +11,11 @@
 # 
 # Fish population dynamics:
 # X_{t+1} = Z_n f(X_n) 
-# 
+
+
 rm(list=ls())   # Start wtih clean workspace 
 require(ggplot2) # nicer plotting package
 
-#set.seed(1)
 # load my script defining the SDP functions:
 # determine_SDP_matrix, find_dp_optim, & ForwardSimulate
 source("stochastic_dynamic_programming.R")
@@ -23,28 +23,33 @@ source("stochastic_dynamic_programming.R")
 # Define all parameters 
 delta <- 0.1      # economic discounting rate
 OptTime <- 50     # stopping time
-sigma_g <- 0.2      # Noise in population growth
+sigma_g <- 0.2    # Noise in population growth
 gridsize <- 100   # gridsize (discretized population)
-sigma_m <- .2  # 
-sigma_i <- .0 # 
-interval <- 1
+sigma_m <- .2     # noise in stock assessment measurement
+sigma_i <- .0     # noise in implementation of the quota
+interval <- 1     # period of updating the stock assessment
+
+
 
 # Chose the state equation / population dynamics function
-f <- BevHolt 
-pars <- c(2,4)  
-K <- (pars[1]-1)/pars[2] 
-xT <- 0 
-e_star <- 0
+f <- BevHolt              # Select the state equation
+pars <- c(2,4)            # parameters for the state equation
+K <- (pars[1]-1)/pars[2]  # Carrying capacity 
+xT <- 0                   # boundary conditions
+e_star <- 0               # model's bifurcation point (just for reference)
 
+## An alternative state equation, with allee effect:
 #f <- Myer
 #pars <- c(1, 2, 6) 
 #p <- pars # shorthand 
 #K <- p[1] * p[3] / 2 + sqrt( (p[1] * p[3]) ^ 2 - 4 * p[3] ) / 2
 #xT <- p[1] * p[3] / 2 - sqrt( (p[1] * p[3]) ^ 2 - 4 * p[3] ) / 2 # allee threshold
 #e_star <- (p[1]*sqrt(p[3])-2)/2 ## Bifurcation point 
-x0 <- K
 
-#' define a profit function, price minus cost
+
+x0 <- K # initial condition
+
+#' Define a profit function, price minus cost
 #' @param x is a the grid of state values (profit will evaluate at each of them)
 #' @param h is the current harvest level being considered by the algorithm
 #' @param p price of fish (Note, optimal will scrap xT if price is high enough!) 
@@ -56,34 +61,31 @@ profit <- function(x_grid, h_i, p = 1, c = 0.001){
   sapply(harvest, function(x) max(0, p * x - c / x))
 }
 
-# Set up the grid 
+# Set up the discrete grids
 x_grid <- seq(0, 2*K, length=gridsize)  # population size
-h_grid <- x_grid  # vector of havest levels, use same res as stock
-#h_grid <- seq(0, 2, length=gridsize) # Myers model based on effort!
-
-# Calculate the transition matrix 
-#SDP_Mat <- determine_SDP_matrix(f, pars, x_grid, h_grid, sigma)
-
-require(snowfall)
-sfInit(parallel=TRUE, cpu=4)
-SDP_Mat <- SDP_by_simulation(f, pars, x_grid, h_grid, sigma_g, sigma_m, sigma_i)
+h_grid <- x_grid  # vector of havest levels, use same resolution as for stock
 
 
-# Find the optimum by dynamic programming 
+#######################################################################
+# Calculate the transition matrix                                     #
+#######################################################################
+SDP_Mat <- determine_SDP_matrix(f, pars, x_grid, h_grid, sigma)
+
+## calculate the transition matrix by simulation 
+#require(snowfall) # use parallelization since this can be slow
+#sfInit(parallel=TRUE, cpu=4)
+#SDP_Mat <- SDP_by_simulation(f, pars, x_grid, h_grid, sigma_g, sigma_m, sigma_i)
+
+
+#######################################################################
+# Find the optimum by dynamic programming                             #
+#######################################################################
 opt <- find_dp_optim(SDP_Mat, x_grid, h_grid, OptTime, xT, profit, delta, reward=100)
 
-# What if parameter estimation is inaccurate? (No crashes w/o Allee)
+
+# What if parameter estimation is inaccurate? e.g.:
 #pars[1] <- pars[1]*.95
 
-
-## Example plot the results of a single run, against unharvested version  
-out <- ForwardSimulate(f, pars, x_grid, h_grid, sigma_g, x0, opt$D,
-                       sigma_g, sigma_i, interval=1)
-dat <- melt(out, id="time")
-p0 <- ggplot(dat, aes(time, value, color=variable)) + geom_line() +  
-  geom_abline(intercept=opt$S, slope=0, col="black") + # Reed's S,
-  geom_abline(intercept = xT, slope=0, col="darkred", lty=3) # unfished Allee 
-p0 <- p0 + geom_abline(intercept=e_star, slope=0, col="green", lty = 2) # tippt
 
 #######################################################################
 # Now we'll simulate this process many times under this optimal havest#
@@ -93,12 +95,16 @@ sims <- lapply(1:100, function(i){
   ForwardSimulate(f, pars, x_grid, h_grid, sigma_g, x0, opt$D, 
                     sigma_m, sigma_i, interval=1)
 })
-dat <- melt(sims, id="time")
 
 
 
 
-# some stats, geom-smooth should do this?
+########################################################################
+#  Summarize and plot data                                             #
+########################################################################
+dat <- melt(sims, id="time") # reshapes the data matrix to "long" form
+
+# some stats on the replicates, ((geom_smooth should do this?))
 m <- cast(dat, time ~ variable, mean) # mean population
 err <- cast(dat, time ~ variable, sd) # sd population
 m_f <- m$fishstock
@@ -106,68 +112,32 @@ err_f <- err$fishstock
 m_h <- m$harvest
 err_h <- err$harvest 
 
+
+## Creat the plot
 p1 <- ggplot(dat) +
       # Replicate harvested dynamics
       geom_line(aes(time, value, group = L1), data = 
-                subset(dat, variable == "fishstock"), alpha = 0.4) + 
+                subset(dat, variable == "fishstock"), alpha = 0.2) + 
       ## Mean & SD for population
       geom_ribbon(aes(x = time, ymin = m_f - err_f, ymax = m_f + err_f),
                   fill = "darkblue", alpha = 0.7)  +
       geom_line(aes(time, m_f), col = "lightblue")  +
-#      geom_abline(intercept=opt$S, slope = 0) + # Reed's S: optimal escapement 
-      geom_abline(intercept=xT, slope = 0, lty=2) + # Allee threshold
-      ## And the same for harvest levels
-      geom_line(aes(time, value, group = L1), 
-            data = subset(dat, variable == "harvest"),  alpha=.2, col = "darkgreen") +
+#      geom_abline(intercept=opt$S, slope = 0) + # show Reed's S: optimal escapement 
+      geom_abline(intercept=xT, slope = 0, lty=2) + # show Allee threshold
+      ## And the same for harvest-effort levels
+#      geom_line(aes(time, value, group = L1), 
+#            data = subset(dat, variable == "harvest"),  alpha=.2, col = "darkgreen") +
       geom_ribbon(aes(x = time, ymin = m_h - err_h, ymax = m_h + err_h),
-                  fill = "darkgreen", alpha = 0.4)  +
-      geom_line(aes(time, m_h), col = "lightgreen")  +
-      geom_abline(intercept = e_star, slope = 0, col = "lightgreen", lwd=1,lty=2) 
+                  fill = "darkgreen", alpha = 0.2)  +
+      geom_line(aes(time, m_h), col = "lightgreen")  #+
+#     geom_abline(intercept = e_star, slope = 0, col = "lightgreen", lwd=1,lty=2) 
 
+
+## Count how many crashed and add it in a plot title
 optimal_crashed = subset(dat, variable == "fishstock" & 
                          time == OptTime-1 & value < xT)
-p1 <- p1 + opts(title=sprintf("Optimal Harvest dynamics, %d populations crash",
-  dim(optimal_crashed)[1]))
+p1 <- p1 + opts(title = sprintf("Optimal Harvest dynamics, %d populations crash",
+                                dim(optimal_crashed)[1]))
 print(p1)
 
-## make the crashed trajectories stand out?
-#p1 <- p1 + geom_line(aes(time, value, group = L1), 
-#          data = subset(dat, variable == "harvest" & 
-#          (L1 %in% optimal_crashed$L1)),  col = "darkgreen", alpha = 0.5) +
-#     geom_line(aes(time, value, group = L1), 
-#          data = subset(dat, variable == "fishstock" &
-#          (L1 %in% optimal_crashed$L1)),  col = "darkblue", alpha = 0.5) 
-
-
-crashed = subset(dat, variable =="unharvested" & time == OptTime-1 & value < xT)
-p2 <- ggplot(dat) + 
-  geom_line(aes(time, value, group = L1), 
-            data = subset(dat, variable == "unharvested"), alpha=.2) + 
-  geom_line(aes(time, cast(dat, time ~ variable, mean)$unharvested)) 
-
-p2 <- p2 + opts(title=sprintf("Unfished dynamics, %d populations crash",
-  dim(crashed)[1]))
-
-
-## Profits plot
-p3 <- ggplot(subset(dat, variable == "harvest"), aes(profit(x_grid,value))) + geom_histogram()
-
-
-# calculate end profits:
-d <- subset(dat, variable == "harvest") 
-income <- profit(x_grid,d$value)
-
-print(p3)
-
-
-#ggsave("samplerun.png", plot=p0)
-#ggsave("fished.png", plot=p1)
-#ggsave("unfished.png", plot=p2)
-
-
-# check out the deterministic dynamics
-x <- numeric(OptTime)
-x[1] <- K
-for(t in 1:(OptTime-1))
-  x[t+1] <- f(x[t],e_star+.01,pars)
 
