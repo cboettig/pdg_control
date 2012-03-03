85bdd42cf1c1f26d74e2a2b1f5bf2150481de3c8
Carl Boettiger <cboettig@gmail.com>
2012-03-03 11:18:48 -0800

    updated profit functions to allow negatives

--- R/profit_functions.R
+++ R/profit_functions.R
@@ -1,10 +1,14 @@
 #' Define a profit function, price minus cost
-#' @param p price of fish (Note, optimal will scrap xT if price is high enough!) 
-#' @param c fishing extraction costs (per unit effort)
-#' @return a function computing profit at effort intensity h_i over
+#' @param price_fish market price (Note, optimal will scrap xT if price is
+#' high enough!) 
+#' @param cost_stock_effect fishing extraction costs (per unit effort)
+#' @param operating_cost costs of having a given harvest level (must
+#' be less than price) 
+##' @return a function computing profit at effort intensity h_i over
 #' possible stock values x_grid, profit(x_grid, h_i)
 #' @export
-profit_effort <- function(p = 1, c = 0.001){
+profit_effort <-  function(price_fish = 1, cost_stock_effect = 0.000,
+                            operating_cost = 0.1 * p){
 #' @param x_grid is a the grid of state values (profit will evaluate at each of them)
 #' @param h_i is the current harvesting *effort* (effort*stocksize = catch) 
 #' @return the profits of fishing at intensity h_i given the stock value equals x_i
@@ -12,20 +16,25 @@ profit_effort <- function(p = 1, c = 0.001){
 #' @details Due to the symmetry, you can actually compute the profit over a range 
 #'  of harvest values, rather than a range of stock values, by simply swapping 
 #'  x and h, i.e. give a vector of h values as x_grid, and a single stock size as h_i. 
-  function(x_grid, h_i){ 
-    harvest <- x_grid * h_i 
-    sapply(harvest, function(x) max(0, p * x - c / x))
+    function(x_grid, h_i){ 
+      sapply(x_grid, function(x_i){
+        h <- h_i * x_i # harvest propotional to effort 
+        price_fish * min(h, x_i) - operating_cost * h  - stock_effect / (x_i + 1e-12)
+    })
   }
 }
 
-
 #' Define a profit function, price minus cost
-#' @param p price of fish (Note, optimal will scrap xT if price is high enough!) 
-#' @param c fishing extraction costs (per unit effort)
+#' @param price_fish market price (Note, optimal will scrap xT if price is
+#' high enough!) 
+#' @param cost_stock_effect fishing extraction costs (per unit effort)
+#' @param operating_cost costs of having a given harvest level (must
+#' be less than price) 
 #' @return a function computing profit at harvest intensity h_i over
 #' possible stock values x_grid, profit(x_grid, h_i)
 #' @export
-profit_harvest  <- function(p = 1, c = 0.000){
+profit_harvest  <- function(price_fish = 1, cost_stock_effect = 0.000,
+                            operating_cost = 0.1 * p){
 #' @param x_grid is a the grid of state values (profit will evaluate at each of them)
 #' @param h_i is total harvest level
 #' @return the profits of harvesting at intensity h_i for each possible stock
@@ -34,9 +43,6 @@ profit_harvest  <- function(p = 1, c = 0.000){
 #'  of harvest values, rather than a range of stock values, by simply swapping 
 #'  x and h, i.e. give a vector of h values as x_grid, and a single stock size as h_i. 
   function(x_grid, h_i){
-    price_fish <- p
-    stock_effect <- c
-    operating_cost <- .1 * p ## doesn't matter if we gonna take max(0,profit)
     sapply(x_grid, function(x_i){
       price_fish * min(h_i, x_i) - operating_cost * h_i  - stock_effect / (x_i + 1e-12)
       # 1e-12 to avoid NaNs at zero stock condition
