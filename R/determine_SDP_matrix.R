af7389c7bd4856681680a655ade9d5b62908e63b
Carl Boettiger <cboettig@gmail.com>
2012-04-30 16:48:01 -0700

    Sethi example seperately

--- R/determine_SDP_matrix.R
+++ R/determine_SDP_matrix.R
@@ -124,7 +124,7 @@ SDP_by_simulation <- function(f, p, x_grid, h_grid, z_g, z_m, z_i, reps = 999){
   SDP_Mat <- sfLapply(h_grid, function(h){ 
     mat <- sapply(x_grid, function(x){
       x_t1 <- replicate(reps, z_g() * z_i() * f(x, z_m() * h, p)) 
-      a <- bin( x_t1, binwidth=bw, range=c(lower, upper))$count
+      a <- mybin( x_t1, binwidth=bw, range=c(lower, upper))$count
       a / sum(a) 
     })
     t(mat)
@@ -132,4 +132,72 @@ SDP_by_simulation <- function(f, p, x_grid, h_grid, z_g, z_m, z_i, reps = 999){
   SDP_Mat
 }
 
+
+
+
+
+# Bin data from old version of ggplot2
+# This function powers \code{\link{stat_bin}}R
+#
+# @keyword internal
+bin <- function(x, weight=NULL, binwidth=NULL, origin=NULL, breaks=NULL, range=NULL, width=0.9, drop = FALSE) {
+  
+  if (is.null(weight))  weight <- rep(1, length(x))
+  weight[is.na(weight)] <- 0
+
+  if (is.null(range))    range <- range(x, na.rm = TRUE, finite=TRUE)
+  if (is.null(binwidth)) binwidth <- diff(range) / 30
+
+  if (is.integer(x)) {
+    bins <- x
+    x <- sort(unique(bins))
+    width <- width    
+  } else if (diff(range) == 0) {
+    width <- width
+    bins <- x
+  } else { # if (is.numeric(x)) 
+    if (is.null(breaks)) {
+      if (is.null(origin)) {
+        breaks <- fullseq(range, binwidth)        
+      } else {
+        breaks <- seq(origin, max(range) + binwidth, binwidth)
+      }
+    }
+    bins <- cut(x, sort(breaks), include.lowest=TRUE)
+    left <- breaks[-length(breaks)]
+    right <- breaks[-1]
+    x <- (left + right)/2
+    width <- diff(breaks)
+  }
+
+  results <- data.frame(
+    count = as.numeric(tapply(weight, bins, sum, na.rm=TRUE)),
+    x = x,
+    width = width
+  )
+
+  res <- within(results, {
+    count[is.na(count)] <- 0
+    density <- count / width / sum(count, na.rm=TRUE)
+    ncount <- count / max(count, na.rm=TRUE)
+    ndensity <- density / max(density, na.rm=TRUE)
+  })
+  if (drop) res <- subset(res, count > 0)
+  res
+}
+
+# Generate sequence of fixed size intervals covering range
+# All locations are multiples of size
+# 
+# @arguments range
+# @arguments interval size
+# @keyword internal
+# @seealso \code{\link{reshape}{round_any}}
+fullseq <- function(range, size) {
+  seq(
+    round_any(range[1], size, floor), 
+    round_any(range[2], size, ceiling), 
+    by=size
+  )
+}
 
