62a38c208b1861ab6d902ebc56486ef3b2c6528e
Carl Boettiger <cboettig@gmail.com>
2012-09-04 15:46:50 -0700

    also manage the queue scripts for farm in git

---
+++ inst/examples/mpi.sh
@@ -0,0 +1,17 @@
+## Use the current working directory
+#$ -cwd
+## use bash commands
+#$ -S /bin/bash
+## combine error and output files
+#$ -j y
+## Parallel for MPI
+##$ -pe mpi 20 
+## Parallel for openmp:
+#$ -pe threaded 16
+## Output file name
+#$ -o det_figure3.log
+
+module load gcc openmpi R 
+Rscript -e "source('~/.Rprofile'); library('knitr'); knit('figure3.Rmd')" 
+
+
