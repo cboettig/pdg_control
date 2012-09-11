474d1f3742475022ba6ac2c3d50b83f444bde694
Carl Boettiger <cboettig@gmail.com>
2012-09-10 18:44:15 -0700

    Sethi analytic integral notes

--- R/SDP_uniform.R
+++ R/SDP_uniform.R
@@ -18,15 +18,12 @@
 #' $F(x,q) = \frac{1}{4\sigma_i\sigma_m} \int_{q-\sigma_i}^{q+\sigma_i} dh \int_{x-\sigma_m}^{x+\sigma_m} f(y, h)$
 #' For logistic map $f(y,h) = 2 (y-h) \left(1-\frac{y-h}{2K}\right)$, this is: 
 #' F <- function(x,q, m, n) 4*m*n*(6*x*K-6*q*K-3*x^2 + 6*q*x - 3*q^2-n^2-m^2) / (3*K)
-#' (where we've used m = \sigma_m, n = \sigma_i for simplicity of Roman notation)
+#' (where we've used m = $\sigma_m$, n = $\sigma_i$ for simplicity of Roman notation)
 #' @export
 SDP_uniform <- function(f, p, x_grid, h_grid, sigma_g,
                         pdfn=function(P, s) dunif(P, 1-s, 1+s),
                         sigma_m, sigma_i, 
-                        f_int = function(x,q, m, n, pars){
-                          K <- pars[2]
-                          4*m*n*(6*x*K-6*q*K-3*x^2 + 6*q*x - 3*q^2-n^2-m^2) / (3*K)
-                        }
+                        f_int = 
                          ){
   gridsize <- length(x_grid)
   SDP_Mat <- lapply(h_grid, function(h){
@@ -54,3 +51,25 @@ SDP_uniform <- function(f, p, x_grid, h_grid, sigma_g,
 }
 
 
+require(cubature)
+
+f_int <- function(f,x,q,sigma_m, sigma_i, pars){
+  g <- function(X) f(X[1], X[2], pars)
+  lower <- c(max(x-sigma_m,0), max(q-sigma_i,0))
+  upper <- c(x+sigma_m, q + sigma_i)
+  out <- adaptIntegrate(g, lower, upper)
+  out$integral
+}
+
+
+F <- function(x,q, m, n, pars){
+  K <- pars[2]
+  ((q+n-max(0,q-n))*(x+m-max(0,x-m))*(6*x*K-6*q*K-6*n*K+6*m*K+6*max(0,x-m)*K-6*
+    max(0,q-n)*K-2*x^2+3*q*x+3*n*x-4*m*x-2*max(0,x-m)*x+3*max(0,q-n)*x-2*q^2-4*n*q+3*m*q+3*
+    max(0,x-m)*q-2*max(0,q-n)*q-2*n^2+3*m*n+3*max(0,x-m)*n-2*max(0,q-n)*n-2*m^2-2*max(0,x-m)*m+3*
+    max(0,q-n)*m-2*max(0,x-m)^2+3*max(0,q-n)*max(0,x-m)-2*max(0,q-n)^2))/(6*K)
+  
+}
+# Confirm that these give the same value
+# f_int(f, 10, 1, .1, .1, pars)
+# F(10, 1, .1, .1, pars)
