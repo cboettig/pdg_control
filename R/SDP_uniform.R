06bba3da4d506a2dd9306578c6ae0b6df295851a
Carl Boettiger <cboettig@gmail.com>
2012-09-10 18:29:24 -0700

    Looking into doing integral analytically and quasi-analytically for the uniform case

---
+++ R/SDP_uniform.R
@@ -0,0 +1,56 @@
+#' Determine the SDP matrix for uniform noise under multiple uncertainty
+#'
+#' Computes the transition matrix under the case of growth noise, 
+#' implementation errors in harvesting, and meaurement errors in 
+#' the stock assessment.  Assumes noise sources are distributed 
+#' by uniform distributions to obtain analytic transition probability densities.
+#'
+#' @param f the growth function of the escapement population (x-h)
+#'   should be a function of f(t, y, p), with parameters p
+#' @param p the parameters of the growth function
+#' @param x_grid the discrete values allowed for the population size, x
+#' @param h_grid the discrete values of harvest levels to optimize over
+#' @param sigma_g is the shape parameter (width) of the multiplicitive growth noise 
+#' @param pdfn is the shape of the growth noise, which need not be uniform (is by default)
+#' @param sigma_m is the half-width of the uniform measurement error (assumes uniform distribution)
+#' @param sigma_i is the half-width of the implementation noise (always assumes uniform distribution)
+#' @param f_int is the function given by the analytic solution,  
+#' $F(x,q) = \frac{1}{4\sigma_i\sigma_m} \int_{q-\sigma_i}^{q+\sigma_i} dh \int_{x-\sigma_m}^{x+\sigma_m} f(y, h)$
+#' For logistic map $f(y,h) = 2 (y-h) \left(1-\frac{y-h}{2K}\right)$, this is: 
+#' F <- function(x,q, m, n) 4*m*n*(6*x*K-6*q*K-3*x^2 + 6*q*x - 3*q^2-n^2-m^2) / (3*K)
+#' (where we've used m = \sigma_m, n = \sigma_i for simplicity of Roman notation)
+#' @export
+SDP_uniform <- function(f, p, x_grid, h_grid, sigma_g,
+                        pdfn=function(P, s) dunif(P, 1-s, 1+s),
+                        sigma_m, sigma_i, 
+                        f_int = function(x,q, m, n, pars){
+                          K <- pars[2]
+                          4*m*n*(6*x*K-6*q*K-3*x^2 + 6*q*x - 3*q^2-n^2-m^2) / (3*K)
+                        }
+                         ){
+  gridsize <- length(x_grid)
+  SDP_Mat <- lapply(h_grid, function(h){
+    SDP_matrix <- matrix(0, nrow=gridsize, ncol=gridsize)
+    # Cycle over x values
+    for(i in 1:gridsize){ 
+      ## Calculate the expected transition  
+      x1 <- x_grid[i]
+      x2_expected <- f_int(x1, h, sigma_m, sigma_i,p)
+      ## If expected 0, go to 0 with probabilty 1
+      if( x2_expected == 0) 
+        SDP_matrix[i,1] <- 1  
+      else {
+        # relative probability of a transition to that state
+        ProportionalChance <- x_grid / x2_expected
+        Prob <- pdfn(ProportionalChance, sigma_g)
+
+        # Store normalized probabilities in row
+        SDP_matrix[i,] <- Prob/sum(Prob)
+      }
+    }
+    SDP_matrix
+  })
+  SDP_Mat
+}
+
+
