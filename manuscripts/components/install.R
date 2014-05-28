c6d6ce1780fc27f89ef3487b46389272164da2db
Carl Boettiger <cboettig@gmail.com>
2014-05-28 14:06:40 -0700

    update documentation, add rmarkdown header info

--- manuscripts/components/install.R
+++ manuscripts/components/install.R
@@ -1,8 +1,12 @@
 #!/usr/bin/Rscript
 
 if(!require("pdgControl")){
-#  install.packages("devtools")
-#  library("devtools")
-#  install_github("reshape")
-  install.packages("..", repos =NULL, dependencies = c("Depends", "Imports", "Suggests"))
+  install.packages("devtools")
+  library("devtools")
+  install_github("reshape")
+  install_github("rmarkdown", "rstudio")
+  install_github("cboettig/cboettigR")
+
+
+  install.packages("..", repos = NULL, dependencies = c("Depends", "Imports", "Suggests"))
 }
