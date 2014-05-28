10afa52aeb1545e7c03dea3b74a9df05a05da654
Carl Boettiger <cboettig@gmail.com>
2014-05-28 14:44:42 -0700

    use github based install of package itself

--- manuscripts/components/install.R
+++ manuscripts/components/install.R
@@ -6,7 +6,5 @@ if(!require("pdgControl")){
   install_github("reshape")
   install_github("rmarkdown", "rstudio")
   install_github("cboettig/cboettigR")
-
-
-  install.packages("..", repos = NULL, dependencies = c("Depends", "Imports", "Suggests"))
+  install_github("cboettig/pdg_control", dependencies = c("Depends", "Imports", "Suggests"))
 }
