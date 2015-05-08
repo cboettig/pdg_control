f328aee8e0e44d46708e68ec8b9a78d8a19a8368
Carl Boettiger <cboettig@gmail.com>
2015-05-08 09:47:57 -0700

    update supplement

--- manuscripts/components/quad_costs.R
+++ manuscripts/components/quad_costs.R
@@ -1,44 +1,4 @@
-parallel = TRUE
-ncpu <- parallel::detectCores()
-
-library("pdgControl")
-library("tidyr")
-library("dplyr")
-library("ggplot2")
-library("reshape2")
-library("data.table")
-library("snowfall")
-
-## ----profit_model--------------------------------------------------------
-price = 10
-c0 = 30
-profit <- profit_harvest(price = price, c0 = c0, c1 = 0)
 profit_quad <- function(c2) profit_harvest(price = price, c0 = c0, c1 = c2)
-
-
-## ----c2_grid-------------------------------------------------------------
-c2 <- exp(seq(0, log(41), length.out = 40))-1
-c2 <- seq(0, 40, length.out=100)
-reduction <- 0.25 
-
-## ----setup---------------------------------------------------------------
-seed <- 123                 # Random seed (replicable results)
-delta <- 0.05               # economic discounting rate
-OptTime <- 20               # stopping time
-gridsize <- 50              # grid size for fish stock and harvest rate (discretized population)
-sigma_g <- 0.2              # Noise in population growth
-reward <- 0                 # bonus for satisfying the boundary condition
-z_g <- function() rlnorm(1,  0, sigma_g) # mean 1
-z_m <- function() 1         # No measurement noise, 
-z_i <- function() 1         # No implemenation noise
-f <- BevHolt                # Select the state equation
-pars <- c(1.5, 0.05)        # parameters for the state equation
-K <- (pars[1] - 1)/pars[2]  # Carrying capacity (for reference 
-xT <- 0                     # boundary conditions
-x0 <- K
-x_grid <- seq(0.01, 1.2 * K, length = gridsize)  
-h_grid <- seq(0.01, 0.8 * K, length = gridsize)  
-
 
 ## ----reed, dependson=c("setup", "profit_model")--------------------------
 SDP_Mat <- determine_SDP_matrix(f, pars, x_grid, h_grid, sigma_g )
@@ -57,7 +17,6 @@ policies <- sfLapply(c2, function(c2){
   policy <- optim_policy(SDP_Mat, x_grid, h_grid, OptTime, xT, 
                          profit_quad(c2), delta, reward, penalty = none)
 })
-
 
 fee <- sapply(policies, function(policy) max(policy$V[i,]))
 npv0 <- fee[1]
@@ -84,13 +43,4 @@ sims <- lapply(reps, function(x)
 dat <- melt(sims, id=names(sims[[1]]))  
 dt_quad <- data.table(dat)
 setnames(dt_quad, "L1", "replicate") # names are nice
-
-
-
-fraction_no_shift <- function(x){
-  noshift <- x[2:length(x)] - x[1:length(x)-1] == 0
-  strictly_positive <- (x > 0.01 )[-1] 
-  who <- noshift & strictly_positive 
-  sum(who)/length(who)
-}
 
